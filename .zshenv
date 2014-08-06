c5d5e07b089001d73b5303943896debb76768519
mediba-kitada <kitada@mediba.jp>
2014-08-06 20:06:59 +0900

    boxen利用時の不具合対応

---
+++ .zshenv
@@ -0,0 +1,1 @@
+export PATH=/usr/local/bin:/opt/boxen/bin:/opt/boxen/homebrew/bin:$PATH
--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-[[ -e ~/.dotfiles ]] || git clone git@github.com:mediba-Kitada/dotfiles.git .dotfiles
-pushd ~/.dotfiles
-
 git submodule init
 git submodule update
 
@@ -13,5 +10,5 @@ do
   [ $i = ".git" ] && continue
   [ $i = "README.md" ] && continue
   [ $i = "install.sh" ] && continue
-  ln -s ~/.dotfiles/$i ~/
+  ln -s ~/dotfiles/$i ~/
 done
