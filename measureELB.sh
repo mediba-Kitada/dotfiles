d5f77b09bc0e8c8a4e29f3354519cbabafebacbe
mediba-kitada <kitada@mediba.jp>
2015-12-02 14:09:25 +0900

    ELBの重要指標取得スクリプトを改修 / 複数ELBに対応

--- measureELB.sh
+++ measureELB.sh
@@ -6,6 +6,8 @@ if ! ([ $1 = "stg" ] || [ $1 = "prd" ]); then
 	exit 1
 fi
 
+# 対象アプリ
+apps=("web" "evt")
 # 対象指標の名称と集計タイプ
 declare -A metrics
 metrics=(["UnHealthyHostCount"]="Sum" ["RequestCount"]="Sum" ["SpilloverCount"]="Sum" ["HTTPCode_ELB_5XX"]="Sum" ["SurgeQueueLength"]="Maximum" ["HTTPCode_Backend_5XX"]="Sum")
@@ -19,14 +21,21 @@ start_time=$2
 if [ -z $start_time ]; then
 	start_time=`date -d '1 hour ago' -u --iso-8601=minutes|cut -d + -f1`
 else
-	start_time=`date -d "${start_time}" -u --iso-8601=minutes|cut -d + -f1`
+	start_time=`date -d "${start_time} hour ago" -u --iso-8601=minutes|cut -d + -f1`
 fi
 [ -z ${start_time} ] && exit 1
 
 # 依存性確認
 ! type aws jq && exit 1
 
-# 一時間の指標をcsvファイルで取得
+# 出力用ディレクトリ
+output_dir="ELB_`date --iso-8601=hours|cut -d + -f1`"
+for app in ${apps[@]}; do
+	! mkdir -p ./${output_dir}/${app} && exit 1
+done;
+
+# 指標をcsvファイルで取得
+for app in ${apps[@]}; do \
 for name in ${!metrics[@]}; do \
 aws --profile ${env} \
 cloudwatch get-metric-statistics --metric-name $name \
@@ -35,6 +44,7 @@ cloudwatch get-metric-statistics --metric-name $name \
 --end-time `date -u --iso-8601=minutes |cut -d + -f1` \
 --namespace AWS/ELB \
 --statistics ${metrics[$name]} \
---dimensions Name=LoadBalancerName,Value=tokuten-web-elb-${env} \
-|jq -r ".Datapoints[]|[.Timestamp,.${metrics[$name]}]|@csv" |sort -n -t , -k 1  >> `date --iso-8601=minutes|cut -d + -f1`_${name}.csv;
+--dimensions Name=LoadBalancerName,Value=tokuten-${app}-elb-${env} \
+|jq -r ".Datapoints[]|[.Timestamp,.${metrics[$name]}]|@csv" |sort -n -t , -k 1  >> ${output_dir}/${app}/${name}.csv;
+done;
 done;
